854c419e1c3f47d95917a3c8203359d05bbc81ba
Josip Krajinovic <krajinov.86@gmail.com>
2017-04-22 14:33:51 +0200

    Adding new project: Arduino - Android Bluetooth LED

---
+++ BluetoothLED/app/src/main/java/com/krajinov/bluetoothled/LedControlActivity.java
@@ -0,0 +1,179 @@
+package com.krajinov.bluetoothled;
+
+import android.app.ProgressDialog;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.res.Configuration;
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.io.IOException;
+import java.util.UUID;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class LedControlActivity extends AppCompatActivity {
+
+    static final UUID myUUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB");
+
+    private String address = null;
+    private ProgressDialog progress;
+    private boolean isBtConnected = false;
+    private BluetoothAdapter myBluetooth = null;
+    private BluetoothSocket btSocket = null;
+
+    @BindView(R.id.btnOn)
+    Button btnOn;
+    @BindView(R.id.btnOff)
+    Button btnOff;
+    @BindView(R.id.btnDisconnect)
+    Button btnDisconnect;
+    @BindView(R.id.seekBar)
+    SeekBar seekBrightness;
+    @BindView(R.id.txtLumn)
+    TextView txtLumn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_led_control);
+        getSupportActionBar().setTitle(getClass().getSimpleName());
+        ButterKnife.bind(this);
+        setOnProgressUpdate();
+
+        address = getIntent().getStringExtra(MainActivity.EXTRA_ADDRESS);
+        ConnectBT connectBT = new ConnectBT();
+        connectBT.execute();
+    }
+
+    private void setOnProgressUpdate() {
+        seekBrightness.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                if (fromUser==true)
+                {
+                    txtLumn.setText(String.valueOf(progress));
+                    try
+                    {
+                        btSocket.getOutputStream().write(String.valueOf(progress).getBytes());
+                    }
+                    catch (IOException e)
+                    {
+
+                    }
+                }
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+
+            }
+        });
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        disconnectBT();
+    }
+
+    @Override
+    public void onConfigurationChanged(Configuration newConfig) {
+        super.onConfigurationChanged(newConfig);
+
+    }
+
+    private class ConnectBT extends AsyncTask<Void, Void, Void>  // UI thread
+    {
+        private boolean ConnectSuccess = true; //if it's here, it's almost connected
+
+        @Override
+        protected void onPreExecute() {
+            progress = ProgressDialog.show(LedControlActivity.this, "Connecting...", "Please wait!!!");  //show a progress dialog
+        }
+
+        @Override
+        protected Void doInBackground(Void... devices) //while the progress dialog is shown, the connection is done in background
+        {
+            try {
+                if (btSocket == null || !isBtConnected) {
+                    myBluetooth = BluetoothAdapter.getDefaultAdapter();//get the mobile bluetooth device
+                    BluetoothDevice dispositivo = myBluetooth.getRemoteDevice(address);//connects to the device's address and checks if it's available
+                    btSocket = dispositivo.createInsecureRfcommSocketToServiceRecord(myUUID);//create a RFCOMM (SPP) connection
+                    BluetoothAdapter.getDefaultAdapter().cancelDiscovery();
+                    btSocket.connect();//start connection
+                }
+            } catch (IOException e) {
+                ConnectSuccess = false;//if the try failed, you can check the exception here
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(Void result) //after the doInBackground, it checks if everything went fine
+        {
+            super.onPostExecute(result);
+
+            if (!ConnectSuccess) {
+                msg("Connection Failed. Is it a SPP Bluetooth? Try again.");
+                finish();
+            } else {
+                msg("Connected.");
+                isBtConnected = true;
+            }
+            progress.dismiss();
+        }
+    }
+
+    private void msg(String s) {
+        Toast.makeText(getApplicationContext(), s, Toast.LENGTH_LONG).show();
+    }
+
+    @OnClick(R.id.btnDisconnect)
+    public void disconnectBT() {
+        if (btSocket != null) //If the btSocket is busy
+        {
+            try {
+                btSocket.close(); //close connection
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+        finish(); //return to the first layout
+    }
+
+    @OnClick(R.id.btnOff)
+    public void turnOffLed() {
+        if (btSocket != null) {
+            try {
+                btSocket.getOutputStream().write("TF".toString().getBytes());
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+    }
+
+    @OnClick(R.id.btnOn)
+    public void turnOnLed() {
+        if (btSocket != null) {
+            try {
+                btSocket.getOutputStream().write("TO".toString().getBytes());
+            } catch (IOException e) {
+                msg("Error");
+            }
+        }
+    }
+}
---
+++ BluetoothLED/app/src/main/java/com/krajinov/bluetoothled/MainActivity.java
@@ -0,0 +1,152 @@
+package com.krajinov.bluetoothled;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Bundle;
+import android.support.v4.content.LocalBroadcastManager;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+import java.util.Set;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class MainActivity extends AppCompatActivity {
+
+    private static final int POWER_ON_BT = 111;
+    public static final String EXTRA_ADDRESS = "MAC_Address";
+
+    private BluetoothAdapter mBluetoothAdapter = null;
+    private Set<BluetoothDevice> pairedDevices;
+    private LocalBroadcastManager broadcastManager;
+    private BroadcastReceiver mReceiver;
+    private ArrayList<String> listOfDevices;
+
+//    @BindView(R.id.btnScan)
+//    Button mBtnScan;
+
+    @BindView(R.id.btnShwDev)
+    Button mBtnShwDew;
+
+    @BindView(R.id.listDev)
+    ListView mListDev;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        initBluetooth();
+    }
+
+    private void initBluetooth() {
+        mBluetoothAdapter = BluetoothAdapter.getDefaultAdapter();
+        if (mBluetoothAdapter == null) {
+            Toast.makeText(this, "Bluetooth not supported", Toast.LENGTH_SHORT).show();
+            finish();
+        } else if (mBluetoothAdapter.isEnabled()) {
+            showPairedDevicesList();
+        } else {
+            //Ask the user to turn the bluetooth on
+            Intent powerOnBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(powerOnBT, POWER_ON_BT);
+        }
+    }
+
+    @OnClick(R.id.btnShwDev)
+    public void showPairedDevicesList() {
+        pairedDevices = mBluetoothAdapter.getBondedDevices();
+        listOfDevices = new ArrayList();
+
+        if (!pairedDevices.isEmpty()) {
+            for (BluetoothDevice bt : pairedDevices) {
+                listOfDevices.add(bt.getName() + "\n" + bt.getAddress()); //Get the device's name and the address
+            }
+        } else {
+            Toast.makeText(this, "No Paired Bluetooth Devices Found.", Toast.LENGTH_LONG).show();
+        }
+        final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, listOfDevices);
+        mListDev.setAdapter(arrayAdapter);
+        mListDev.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // Get the device MAC address, the last 17 chars in the View
+                String info = ((TextView) view).getText().toString();
+                String address = info.substring(info.length() - 17);
+                // Make an intent to start next activity.
+                Intent i = new Intent(MainActivity.this, LedControlActivity.class);
+                //Change the activity.
+                i.putExtra(EXTRA_ADDRESS, address); //this will be received at ledControl (class) Activity
+                startActivity(i);
+            }
+        });
+    }
+
+//    @OnClick (R.id.btnScan)
+//    public void scanBTDevices() {
+//        broadcastManager = LocalBroadcastManager.getInstance(this);
+//        mBluetoothAdapter.startDiscovery();
+//        mReceiver = new BroadcastReceiver() {
+//            public void onReceive(Context context, Intent intent) {
+//                String action = intent.getAction();
+//
+//                //Finding devices
+//                if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+//                    // Get the BluetoothDevice object from the Intent
+//                    BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+//                    String deviceItem = device.getName() + "\n" + device.getAddress();
+//                    if (listOfDevices == null) {
+//                        listOfDevices = new ArrayList<>();
+//                    }
+//                    for (String s : listOfDevices) {
+//                        if (s.equalsIgnoreCase(deviceItem)) {
+//                            return;
+//                        }
+//                    }
+//                    // Add the name and address to an array adapter to show in a ListView
+//                    listOfDevices.add(deviceItem);
+//                }
+//            }
+//        };
+//
+//        IntentFilter filter = new IntentFilter(BluetoothDevice.ACTION_FOUND);
+//        broadcastManager.registerReceiver(mReceiver, filter);
+//    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if (broadcastManager != null) {
+            broadcastManager.unregisterReceiver(mReceiver);
+        }
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i("BluetoothResult", ": " + resultCode);
+        if (requestCode == POWER_ON_BT) {
+            if (resultCode == RESULT_OK) {
+                Toast.makeText(this, "Bluetooth enabled", Toast.LENGTH_SHORT).show();
+            } else {
+                Toast.makeText(this, "Can't proceed! Bluetooth is disabled!", Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+}
